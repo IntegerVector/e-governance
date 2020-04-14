4c4fb230e0ed16480894140fdfc7a97fdfc90ac9
Andrew Ruban <andrew.ryban@gmail.com>
2020-04-14 19:58:07 +0300

    App routing added

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,8 +1,64 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { UrlParamsResolverService } from './shared/services/url-params-resolver/url-params-resolver.service';
+import { MainPageComponent } from './page-components/components/main-page/main-page.component';
+import { UserAuthorizationComponent } from './page-components/components/user-authorization/user-authorization.component';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full'
+  },
+  {
+    path: 'main',
+    component: MainPageComponent,
+    resolve: {
+      urlParams: UrlParamsResolverService
+    }
+  },
+  {
+    path: 'log-in',
+    component: UserAuthorizationComponent,
+    data: {
+      mode: 'logIn'
+    },
+    resolve: {
+      urlParams: UrlParamsResolverService
+    }
+  },
+  {
+    path: 'sign-up',
+    component: UserAuthorizationComponent,
+    data: {
+      mode: 'signUp'
+    },
+    resolve: {
+      urlParams: UrlParamsResolverService
+    }
+  },
+  {
+    path: 'error',
+    component: ErrorComponent,
+    resolve: {
+      urlParams: UrlParamsResolverService
+    }
+  },
+  {
+    path: '**',
+    redirectTo: 'error',
+    resolve: {
+      urlParams: {
+        errCode: '404',
+        errMsg: 'Page Not Found',
+        errTip: 'Try to navigate to the main page'
+      }
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
