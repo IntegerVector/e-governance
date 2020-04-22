90b1da6ae0181c402fb27bdc82215b0777e4265d
Andrew Ruban <andrew.ryban@gmail.com>
2020-04-22 19:39:34 +0300

    FE: new urls added

--- app/src/app/shared/services/data-source/constants/urls.ts
+++ app/src/app/shared/services/data-source/constants/urls.ts
@@ -1,1 +1,8 @@
-export const getUserData = '/get-user-by-id';
+export const getUserDataUrl = '/get-user-by-id';
+export const userLogInUrl = '/user-log-in';
+export const userRegisterUrl = '/user-register';
+export const userUpdateUrl = '/user-update';
+export const getUserDataByUserIdUrl = '/get-user-data-by-user-id';
+export const getUserPermissionsUrl = '/get-user-permissions';
+export const getUsersTypesUrl = '/get-user-types';
+export const getUserStatusesUrl = '/get-user-statuses';
