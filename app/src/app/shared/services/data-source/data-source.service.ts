61f7d726ba10bd1dc54b15e5002ee9effcd8d300
Andrew Ruban <andrew.ryban@gmail.com>
2020-04-22 19:40:22 +0300

    FE: dataSource service new getters added

--- app/src/app/shared/services/data-source/data-source.service.ts
+++ app/src/app/shared/services/data-source/data-source.service.ts
@@ -1,136 +1,200 @@
-import { Injectable, NgModuleFactoryLoader } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subscription, Subscriber } from 'rxjs';
 import { tap, catchError, map } from 'rxjs/operators';
+import { get } from 'lodash';
 
-import { PanelOptionsDTO, PanelOptions } from '../../types/dto/panel-options-dto';
-import { getUserData } from './constants/urls';
+import {
+    getUserDataUrl,
+    userLogInUrl,
+    userRegisterUrl,
+    userUpdateUrl,
+    getUserDataByUserIdUrl,
+    getUserPermissionsUrl,
+    getUsersTypesUrl,
+    getUserStatusesUrl
+} from './constants/urls';
 import { UserDTO, User } from '../../types/dto/user-dto';
 import { BaseRequest } from '../../types/base-request';
-import { RequestTypes } from '../../types/enums/request-type.enum';
-import { DEFAULT_USER_ID, DEFAULT_USER_TOKEN } from '../../shared.constants';
+import { RequestTypesEnum } from '../../types/enums/request-type.enum';
+import { DEFAULT_USER_ID, DEFAULT_USER_TOKEN } from '../../constants/shared.constants';
 import { ErrorHandlerService } from 'src/app/error/services/error-handler.service';
 import { ErrorObjectInterface } from 'src/app/error/types/error-object.interface';
-import { unknownErrorTip } from 'src/app/error/error.constants';
+import { UserData } from '../../types/dto/user-data-dto';
+import { PermissionsEnum } from '../../types/enums/permissions.enum';
+import { DataSaverService } from '../data-saver/data-saver.service';
+import { UserDataInterface } from '../data-saver/types/user-data.interface';
+import { UserType } from '../../types/dto/user-type-dto';
+import { UserStatusEnum } from '../../types/dto/user-status-dto';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class DataSourceService {
-  private userId: string = null;
-  private userTocken: string = null;
-  private lastError: ErrorObjectInterface = {
-    errCode: null,
-    errMsg: null,
-    errTip: null
-  };
-
-  // To delete:
-  private panelOptionsMock: PanelOptions = {
-    left: [
-      {
-        title: 'Test',
-        url: 'test'
-      }
-    ],
-    center: {
-      iconUrl: 'icon-url',
-      label: 'Main Page',
-      url: '/'
-    },
-    right: {
-      buttons: [
-        {
-          title: 'Just Mock button',
-          url: 'fuck'
-        }
-      ],
-      user: {
-        avatarUrl: 'user-avatar',
-        mainLabel: 'Fuck, You',
-        subLabel: 'some sub data'
-      }
-    }
-  };
-
-  constructor(
-    private http: HttpClient,
-    private errorHandlerService: ErrorHandlerService
-  ) { }
-
-  public isUserIdValid(userId: string): boolean {
-    if (!this.userId) {
-      return false;
-    }
-
-    return userId === this.userId ? true : false;
-  }
-
-  public getTokenById(userId: string): string {
-    return userId === this.userId ? this.userTocken : null;
-  }
-
-  public getPanelOptions(): Observable<any> {
-    return of(this.panelOptionsMock);
-  }
-
-  public getUserById(userId): Observable<User> {
-    const requestData = { userId };
-
-    return this.get(getUserData, requestData);
-  }
-
-  private post(url: string, requestData: any): Observable<any> {
-    const body: BaseRequest<any> = {
-      userId: +this.userId,
-      userToken: this.getTokenById(this.userId),
-      type: RequestTypes.post,
-      data: requestData
+    private userId: string = null;
+    private userTocken: string = null;
+    private lastError: ErrorObjectInterface = {
+        errCode: null,
+        errMsg: null,
+        errTip: null
     };
+    private userTypes: UserType[];
+    private userStatuses: UserStatusEnum[];
 
-    return this.makeRequest(url, body);
-  }
-
-  private get(url: string, requestData: any): Observable<any> {
-    const body: BaseRequest<any> = {
-      userId: +this.userId,
-      userToken: this.getTokenById(this.userId),
-      type: RequestTypes.get,
-      data: requestData
-    };
-
-    return this.makeRequest(url, body);
-  }
-
-  private makeRequest(url: string, body: BaseRequest<any>): Observable<any> {
-    return this.http.post(url, body).pipe(
-      tap((data: UserDTO) => this.updateUserData(data.userId, data.userToken)),
-      catchError(this.errorHandler.bind(this)),
-      map(this.mapResponce.bind(this))
-    );
-  }
-
-  private updateUserData(userId: number, userToken: string): void {
-    this.userId = userId.toString() || DEFAULT_USER_ID;
-    this.userTocken = userToken || DEFAULT_USER_TOKEN;
-  }
-
-  private errorHandler(err: any, caught: BaseRequest<any>): Observable<BaseRequest<void>> {
-    this.lastError.errCode = err.status;
-    this.lastError.errMsg = err.statusText;
-    this.lastError.errTip = err.message;
-
-    this.errorHandlerService.navigateToErrPage(this.lastError);
-
-    return of({
-      ...caught,
-      userId: +this.userId,
-      userToken: this.userTocken,
-      error: this.lastError
+    private userIdObservable = new Observable(subscriber => {
+        subscriber.next(DEFAULT_USER_ID);
+        this.userIdSubscriber = subscriber;
     });
-  }
+    private userIdSubscriber: Subscriber<string>;
 
-  private mapResponce(data: BaseRequest<any>): any {
-    return data.data;
-  }
+    constructor(
+        private http: HttpClient,
+        private errorHandlerService: ErrorHandlerService,
+        private dataSaverService: DataSaverService
+    ) { }
+
+    public restoreUserAuthData(): UserDataInterface {
+        const userAuthData = this.dataSaverService.getUserData();
+        this.userId = get(userAuthData, 'userId') || DEFAULT_USER_ID;
+        this.userTocken = get(userAuthData, 'userToken') || DEFAULT_USER_TOKEN;
+
+        if (this.userIdSubscriber) {
+            this.userIdSubscriber.next(this.userId);
+        }
+
+        return {
+            userId: this.userId,
+            userToken: this.userTocken
+        };
+    }
+
+    public isUserIdValid(userId: string): boolean {
+        if (!this.userId) {
+            return false;
+        }
+
+        return userId === this.userId ? true : false;
+    }
+
+    public getId(): string {
+        return this.userId;
+    }
+
+    public subscribeToUserIdChanges(action: (userId: string) => void): Subscription {
+        return this.userIdObservable.subscribe(action);
+    }
+
+    public getTokenById(userId: string): string {
+        return userId === this.userId ? this.userTocken : null;
+    }
+
+    public getUserById(userId): Observable<User> {
+        const requestData = { userId };
+
+        return this.get(getUserDataUrl, requestData);
+    }
+
+    public getUsersTypes(): Observable<UserType[]> {
+        if (this.userTypes) {
+            return of(this.userTypes);
+        }
+
+        return this.get(getUsersTypesUrl, null)
+            .pipe(
+                map(data => data.types),
+                tap(data => this.userTypes = data)
+            );
+    }
+
+    public getUserStatuses(): Observable<UserStatusEnum[]> {
+        if (this.userStatuses) {
+            return of(this.userStatuses);
+        }
+
+        return this.get(getUserStatusesUrl, null)
+            .pipe(
+                map(data => data.statuses),
+                tap(data => this.userStatuses = data)
+            );
+    }
+
+    public getUserDataByUserId(userId: string): Observable<UserData> {
+        const requestData = { userId };
+
+        return this.get(getUserDataByUserIdUrl, requestData);
+    }
+
+    public getPermissions(userId: string): Observable<PermissionsEnum[]> {
+        return this.get(getUserPermissionsUrl, null).pipe(map(data => data.permissions));
+    }
+
+    public signIn(userAuthData: { login: string; pass: string }): Observable<User> {
+        return this.get(userLogInUrl, userAuthData);
+    }
+
+    public registerNewUser(user: User & UserData): Observable<User> {
+        return this.post(userRegisterUrl, user);
+    }
+
+    public updateUser(user: User & UserData): Observable<User> {
+        return this.post(userUpdateUrl, user);
+    }
+
+    private post(url: string, requestData: any): Observable<any> {
+        const body: BaseRequest<any> = {
+            userId: +this.userId,
+            userToken: this.getTokenById(this.userId),
+            type: RequestTypesEnum.post,
+            data: requestData
+        };
+
+        return this.makeRequest(url, body);
+    }
+
+    private get(url: string, requestData: any): Observable<any> {
+        const body: BaseRequest<any> = {
+            userId: +this.userId,
+            userToken: this.getTokenById(this.userId),
+            type: RequestTypesEnum.get,
+            data: requestData
+        };
+
+        return this.makeRequest(url, body);
+    }
+
+    private makeRequest(url: string, body: BaseRequest<any>): Observable<any> {
+        return this.http.post(url, body).pipe(
+            tap((data: UserDTO) => this.updateUserAuthData(data.userId, data.userToken)),
+            catchError(this.errorHandler.bind(this)),
+            map(this.mapResponce.bind(this))
+        );
+    }
+
+    private updateUserAuthData(userId: number, userToken: string): void {
+        this.userId = userId.toString() || DEFAULT_USER_ID;
+        this.userTocken = userToken || DEFAULT_USER_TOKEN;
+
+        if (this.userIdSubscriber) {
+            this.userIdSubscriber.next(this.userId);
+        }
+    }
+
+    private errorHandler(err: any, caught: BaseRequest<any>): Observable<BaseRequest<void>> {
+        this.lastError.errCode = err.status;
+        this.lastError.errMsg = err.statusText;
+        this.lastError.errTip = err.message;
+
+        this.errorHandlerService.navigateToErrPage(this.lastError);
+
+        return of({
+            ...caught,
+            userId: +this.userId,
+            userToken: this.userTocken,
+            error: this.lastError
+        });
+    }
+
+    private mapResponce(data: BaseRequest<any>): any {
+        return data.data;
+    }
 }
