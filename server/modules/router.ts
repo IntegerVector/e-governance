b1dbc32d2e736c3c3ed60a0dfc99983936125683
Andrew Ruban <andrew.ryban@gmail.com>
2020-04-22 19:35:55 +0300

    BE: getting userData files added

--- server/modules/router.ts
+++ server/modules/router.ts
@@ -1,15 +1,15 @@
 import * as express from 'express';
 import * as fs from 'fs';
 import { resolve } from 'path';
-import { ROUTES_FILE_PATH, CLIENT_ROOT, MAIN_PAGE } from '../constants/constants';
+import { ROUTES_FILE_PATH, CLIENT_ROOT, MAIN_PAGE, USER_DATA_FILES_PATH } from '../constants/constants';
 import { Actions } from './action-manager';
-import { Route } from '../types/interfaces/route.interface';
+import { RouteInterface } from '../types/interfaces/route.interface';
 import { LoggerSingleton } from './logger';
 
 const logger = LoggerSingleton.getInstance();
 
 export class Router {
-    private routes: Route[];
+    private routes: RouteInterface[];
 
     constructor(private server: express.Application) {
         this.routes = this.loadRoutes();
@@ -24,19 +24,23 @@ export class Router {
             res.sendFile(resolve(CLIENT_ROOT + MAIN_PAGE));
         });
 
+        this.server.get(USER_DATA_FILES_PATH + '/*', (req, res) => {
+            res.sendFile(resolve(CLIENT_ROOT + req.url));
+        });
+
         this.server.get(/.+\.(html)|(js)|(js\.map)|(ico)/, (req, res) => {
             logger.info('File request:', CLIENT_ROOT + req.url);
             res.sendFile(resolve(CLIENT_ROOT + req.url));
         });
 
-        this.routes.forEach((rout: Route) => {
+        this.routes.forEach((rout: RouteInterface) => {
             this.server.post(rout.url, (req, res) => {
                 Actions.execute(rout.action, rout.type, req, res);
             });
         });
     }
 
-    private loadRoutes(): Route[] {
+    private loadRoutes(): RouteInterface[] {
         const data = fs.readFileSync(ROUTES_FILE_PATH);
         return JSON.parse(data.toString());
     }
