308fd84b304964b10c36b956fb73b2c47515662f
Andrew Ruban <andrew.ryban@gmail.com>
2020-04-22 19:37:50 +0300

    FE: main page basics added

---
+++ app/src/app/page-components/components/main-page/main-page.constants.ts
@@ -0,0 +1,6 @@
+export const headerLabel = 'Wellcome!';
+export const headerTitle = 'wellcome to the fkeeper';
+export const sectionHeaderLabel = 'Main page';
+export const sectionPTextTemplate = 'You are using this page as {0} user';
+export const sectionPDefaultText = 'You are using this page as "Guest" user';
+export const sectionText = 'This application allows you to ...';
