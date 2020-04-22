926f7258ce768aa8c9cc3700aa510c7c21794726
Andrew Ruban <andrew.ryban@gmail.com>
2020-04-22 19:38:52 +0300

    FE: new constants added

---
+++ app/src/app/shared/constants/internal-urls.constants.ts
@@ -0,0 +1,5 @@
+export const MAIN_PAGE_INTERNAL_URL = 'main';
+export const ERROR_PAGE_INTERNAL_URL = 'error';
+export const LOG_IN_INTERNAL_URL = 'log-in';
+export const SIGN_UP_INTERNAL_URL = 'sign-up';
+export const PROFILE_INTERNAL_URL = 'user-profile';
