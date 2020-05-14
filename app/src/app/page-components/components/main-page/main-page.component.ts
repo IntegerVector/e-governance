242e623d065599f7c2f494104d73af920ffb3f16
IntegerVector <andrew.ryban@gmail.com>
2020-05-15 02:29:36 +0300

    FE: main page updated

--- app/src/app/page-components/components/main-page/main-page.component.ts
+++ app/src/app/page-components/components/main-page/main-page.component.ts
@@ -9,11 +9,55 @@ import { ActivatedRoute } from '@angular/router';
 export class MainPageComponent implements OnInit {
     public userId: string;
 
+    public links = [
+        {
+            routerLink: '/main/about',
+            getQueryParams: this.getQueryParams.bind(this),
+            title: 'About',
+            childs: []
+        },
+        {
+            routerLink: '/main/waiting-documents',
+            getQueryParams: this.getQueryParams.bind(this),
+            title: 'Waiting documents',
+            childs: []
+        },
+        {
+            routerLink: '/main/complited-documents',
+            getQueryParams: this.getQueryParams.bind(this),
+            title: 'Complited documents',
+            childs: []
+        },
+        {
+            routerLink: null,
+            getQueryParams: () => null,
+            title: null,
+            childs: [
+                {
+                    routerLink: '/main/about',
+                    getQueryParams: this.getQueryParams.bind(this),
+                    title: 'Academic Vacation'
+                },
+                {
+                    routerLink: '/main/about',
+                    getQueryParams: this.getQueryParams.bind(this),
+                    title: 'Get average mark'
+                }
+            ]
+        }
+    ];
+
     constructor(private route: ActivatedRoute) {}
 
     public ngOnInit(): void {
         this.route.data.subscribe(({ urlParams }) => {
             this.userId = urlParams.userId;
         });
+    }
+
+    public getQueryParams() {
+        return {
+            userId: this.userId
+        };
     }
 }
