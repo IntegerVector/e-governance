d52d8e3bdb84610b80f877cbe72cc0d869bb2e72
IntegerVector <andrew.ryban@gmail.com>
2020-05-16 05:47:57 +0300

    BE: pdf template fixed

--- server/modules/pdf-modules/pdf-manager.ts
+++ server/modules/pdf-modules/pdf-manager.ts
@@ -10,17 +10,7 @@ export async function createPDF(template: string, data: any, user: User): Promis
         orientation: "portrait",
         border: "10mm",
         header: {
-            height: "45mm",
-            contents: `<div style="text-align: center;">Author: ${user.userFirstName} ${user.userLastName}</div>`
-        },
-        "footer": {
-            "height": "28mm",
-            "contents": {
-            first: 'Cover page',
-            2: 'Second page',
-            default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>',
-            last: 'Last Page'
-            }
+            height: "45mm"
         }
     };
 
