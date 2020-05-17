04fa2f7f6005fe0bfcef393d179f13aff219ba15
IntegerVector <andrew.ryban@gmail.com>
2020-05-17 20:05:29 +0300

    FE: main page links updated

--- app/src/app/page-components/components/main-page/main-page.component.ts
+++ app/src/app/page-components/components/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DEFAULT_USER_ID } from 'src/app/shared/constants/shared.constants';
 
 @Component({
     selector: 'app-main-page',
@@ -52,6 +53,12 @@ export class MainPageComponent implements OnInit {
     public ngOnInit(): void {
         this.route.data.subscribe(({ urlParams }) => {
             this.userId = urlParams.userId;
+
+            if (this.userId.toString() === DEFAULT_USER_ID) {
+                this.setGuestLinks();
+            } else {
+                this.setSignedUserLinks();
+            }
         });
     }
 
@@ -59,5 +66,56 @@ export class MainPageComponent implements OnInit {
         return {
             userId: this.userId
         };
+    }
+
+    private setGuestLinks(): void {
+        this.links = [
+            {
+                routerLink: '/main/about',
+                getQueryParams: this.getQueryParams.bind(this),
+                title: 'About',
+                childs: []
+            }
+        ];
+    }
+
+    private setSignedUserLinks(): void {
+        this.links = [
+            {
+                routerLink: '/main/about',
+                getQueryParams: this.getQueryParams.bind(this),
+                title: 'About',
+                childs: []
+            },
+            {
+                routerLink: '/main/waiting-documents',
+                getQueryParams: this.getQueryParams.bind(this),
+                title: 'Waiting documents',
+                childs: []
+            },
+            {
+                routerLink: '/main/complited-documents',
+                getQueryParams: this.getQueryParams.bind(this),
+                title: 'Complited documents',
+                childs: []
+            },
+            {
+                routerLink: null,
+                getQueryParams: () => null,
+                title: null,
+                childs: [
+                    {
+                        routerLink: '/main/academic-vacation',
+                        getQueryParams: this.getQueryParams.bind(this),
+                        title: 'Academic Vacation'
+                    },
+                    {
+                        routerLink: '/main/about',
+                        getQueryParams: this.getQueryParams.bind(this),
+                        title: 'Get average mark'
+                    }
+                ]
+            }
+        ];
     }
 }
