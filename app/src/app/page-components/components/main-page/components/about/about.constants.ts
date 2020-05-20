3ce75f7c117b75cd0949d3f816f516b328da629c
IntegerVector <andrew.ryban@gmail.com>
2020-05-20 23:09:30 +0300

    FE: about page updated

--- app/src/app/page-components/components/main-page/components/about/about.constants.ts
+++ app/src/app/page-components/components/main-page/components/about/about.constants.ts
@@ -1,4 +1,8 @@
 export const sectionHeaderLabel = 'About project';
 export const sectionPTextTemplate = 'You are using this page as {0} user';
 export const sectionPDefaultText = 'You are using this page as "Guest" user';
-export const sectionText = 'This application allows you to ...';
+export const sectionText = `This application allows you to request academic vacation and
+    , in future, to create many other documents and requests.
+    For staring using the service "Log In" into your account.
+    If you haven't user name and password get in contact with your administrator,
+    only administrator allowed to add new users`;
